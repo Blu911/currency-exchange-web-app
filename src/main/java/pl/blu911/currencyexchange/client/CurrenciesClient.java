c56f5034ab399811a372723475351ad6f3372b5b
Krystian Woźniak <krys.blu@gmail.com>
2019-05-22 21:13:27 +0200

    Refactor: HttpClient and CurrenciesClient to get expected content

--- src/main/java/pl/blu911/currencyexchange/client/CurrenciesClient.java
+++ src/main/java/pl/blu911/currencyexchange/client/CurrenciesClient.java
@@ -1,12 +1,31 @@
 package pl.blu911.currencyexchange.client;
 
-import java.io.IOException;
+import com.codesnippets4all.json.parsers.JSONParser;
+import com.codesnippets4all.json.parsers.JsonParserFactory;
+import pl.blu911.currencyexchange.model.Currency;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 public class CurrenciesClient {
 
     private static final String CURRENCIES_URI = "https://openexchangerates.org/api/currencies.json";
 
-    public static String getFileContent() throws IOException {
-        return HttpClient.getStringFromUri(CURRENCIES_URI);
+    public static List<Currency> getCurrencies() {
+        String jsonString = HttpClient.getStringFromUri(CURRENCIES_URI);
+
+        JsonParserFactory factory = JsonParserFactory.getInstance();
+        JSONParser parser = factory.newJsonParser();
+        Map jsonMap = parser.parseJson(jsonString);
+
+        List<Currency> currencyList = new ArrayList<>();
+
+        jsonMap.forEach((key, value) -> {
+            currencyList.add(new Currency((String) key, (String) value));
+        });
+
+        currencyList.sort(Comparator.comparing(Currency::getCode));
+        return currencyList;
     }
 }
--- src/main/java/pl/blu911/currencyexchange/client/HttpClient.java
+++ src/main/java/pl/blu911/currencyexchange/client/HttpClient.java
@@ -1,12 +1,20 @@
 package pl.blu911.currencyexchange.client;
 
 import com.jcabi.http.request.JdkRequest;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
-public class HttpClient {
+class HttpClient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrenciesClient.class);
 
-    static String getStringFromUri(String uri) throws IOException {
-        return new String(new JdkRequest(uri).fetch().binary());
+    static String getStringFromUri(String uri) {
+        try {
+            return new String(new JdkRequest(uri).fetch().binary());
+        } catch (IOException e) {
+            LOGGER.error("Error while connecting to external URI");
+            e.printStackTrace();
+        }
+        return null;
     }
 }
