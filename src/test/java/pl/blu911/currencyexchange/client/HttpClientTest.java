66cca2cd35cbfb5c33ddf3fa1b5c7094fd97d0f2
Krystian Woźniak <krys.blu@gmail.com>
2019-05-28 21:22:55 +0200

    Test: HttpClient should return null when file not found

--- src/test/java/pl/blu911/currencyexchange/client/HttpClientTest.java
+++ src/test/java/pl/blu911/currencyexchange/client/HttpClientTest.java
@@ -1,16 +1,23 @@
 package pl.blu911.currencyexchange.client;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
 import static org.junit.Assert.*;
 
 public class HttpClientTest {
+private HttpClient httpClient;
+
+    @Before
+    public void setUp() {
+        httpClient = new HttpClient();
+    }
 
     @Test
-    public void shouldReturnExpectedString() throws IOException {
-        String returnedString = HttpClient.getStringFromUri("https://support.oneskyapp.com/hc/en-us/article_attachments/202761627/example_1.json");
+    public void shouldReturnExpectedString() {
+        String uri = "https://support.oneskyapp.com/hc/en-us/article_attachments/202761627/example_1.json";
+        String returnedString = httpClient.getStringFromUri(uri);
 
         String expectedString = "{\n" +
                 "    \"fruit\": \"Apple\",\n" +
@@ -20,4 +27,14 @@ public class HttpClientTest {
 
         assertThat(returnedString, Matchers.is(expectedString));
     }
+
+    @Test
+    public void shouldReturnNullWhenNoItemFound() {
+        String uri = "https://support.oneskyapp.com/hc/en-us/article_attachments/202761627/example_2.json";
+
+        String returnedString = httpClient.getStringFromUri(uri);
+
+        assertNull(returnedString);
+    }
+
 }
